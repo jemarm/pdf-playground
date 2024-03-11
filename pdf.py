3c9cc810072ad526a0b6a50b3c22d1f20d97cdcc
JayM647 <jemarmrrs@gmail.com>
2024-03-10 23:12:15 -0400

    worked on functionality of merging pdfs. created script to add bookmark to each pdf page

--- pdf.py
+++ pdf.py
@@ -1,10 +1,37 @@
 import PyPDF2
 
-with open('dummy.pdf', 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    page = reader.getPage(0)
-    page.rotateCounterClockwise(90)
-    writer = PyPDF2.PdfFileWriter()
-    writer.addPage(page)
-    with open('tilt.pdf', 'wb') as new_file:
-        writer.write(new_file)
+
+def add_watermark(super_pdf, wtrmrk_pdf, marked_pdf):
+    with open(super_pdf, 'rb') as super_file:
+        super_pdf_reader = PyPDF2.PdfFileReader(super_file)
+        with open(wtrmrk_pdf, 'rb') as wtr_file:
+            wtrmrk_pdf_reader = PyPDF2.PdfFileReader(wtr_file)
+            marked_pdf_writer = PyPDF2.PdfFileWriter()
+            wrtmrk_page = wtrmrk_pdf_reader.getPage(0)
+            for page_num in range(super_pdf_reader.numPages):
+                page = super_pdf_reader.getPage(page_num)
+                page.mergePage(wrtmrk_page)
+                marked_pdf_writer.addPage(page)
+            with open(marked_pdf, 'wb') as marked_file:
+                marked_pdf_writer.write(marked_file)
+
+
+# Hardcoded file names
+super_pdf = 'super.pdf'
+wtrmrk_pdf = 'wtr.pdf'
+marked_pdf = 'marked.pdf'
+
+add_watermark(super_pdf, wtrmrk_pdf, marked_pdf)
+
+
+# inputs = sys.argv[1:]
+
+# def pdf_combiner(pdf_list):
+#     merger = PyPDF2.PdfFileMerger()
+#     for pdf in pdf_list:
+#         print(pdf)
+#         merger.append(pdf)
+#     merger.write('super.pdf')
+
+
+# pdf_combiner(inputs)
