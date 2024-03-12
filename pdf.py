eafc63a07d38f68b9696fcb3d28e4dc09572893a
JayM647 <jemarmrrs@gmail.com>
2024-03-12 08:01:44 -0400

    updated with instructor solution

--- pdf.py
+++ pdf.py
@@ -1,27 +1,40 @@
 import PyPDF2
 
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
 
-def add_watermark(super_pdf, wtrmrk_pdf, marked_pdf):
-    with open(super_pdf, 'rb') as super_file:
-        super_pdf_reader = PyPDF2.PdfFileReader(super_file)
-        with open(wtrmrk_pdf, 'rb') as wtr_file:
-            wtrmrk_pdf_reader = PyPDF2.PdfFileReader(wtr_file)
-            marked_pdf_writer = PyPDF2.PdfFileWriter()
-            wrtmrk_page = wtrmrk_pdf_reader.getPage(0)
-            for page_num in range(super_pdf_reader.numPages):
-                page = super_pdf_reader.getPage(page_num)
-                page.mergePage(wrtmrk_page)
-                marked_pdf_writer.addPage(page)
-            with open(marked_pdf, 'wb') as marked_file:
-                marked_pdf_writer.write(marked_file)
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
 
 
-# Hardcoded file names
-super_pdf = 'super.pdf'
-wtrmrk_pdf = 'wtr.pdf'
-marked_pdf = 'marked.pdf'
+# my solution
+# def add_watermark(super_pdf, wtrmrk_pdf, marked_pdf):
+#     with open(super_pdf, 'rb') as super_file:
+#         super_pdf_reader = PyPDF2.PdfFileReader(super_file)
+#         with open(wtrmrk_pdf, 'rb') as wtr_file:
+#             wtrmrk_pdf_reader = PyPDF2.PdfFileReader(wtr_file)
+#             marked_pdf_writer = PyPDF2.PdfFileWriter()
+#             wrtmrk_page = wtrmrk_pdf_reader.getPage(0)
+#             for page_num in range(super_pdf_reader.numPages):
+#                 page = super_pdf_reader.getPage(page_num)
+#                 page.mergePage(wrtmrk_page)
+#                 marked_pdf_writer.addPage(page)
+#             with open(marked_pdf, 'wb') as marked_file:
+#                 marked_pdf_writer.write(marked_file)
 
-add_watermark(super_pdf, wtrmrk_pdf, marked_pdf)
+
+# # Hardcoded file names
+# super_pdf = 'super.pdf'
+# wtrmrk_pdf = 'wtr.pdf'
+# marked_pdf = 'marked.pdf'
+
+# add_watermark(super_pdf, wtrmrk_pdf, marked_pdf)
 
 
 # inputs = sys.argv[1:]
